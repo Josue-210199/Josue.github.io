2bd8e646bba6b5d1c476e913b5ed036fdd27fb1a
Josue-210199 <jv2405471@gmail.com>
2025-10-07 23:39:34 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,22 @@
-self.addEventListener('install', event => {
+const CACHE_NAME = "jb-sig-studio-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./puntos.js",
+  "https://unpkg.com/leaflet/dist/leaflet.css",
+  "https://unpkg.com/leaflet/dist/leaflet.js"
+];
+
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open('jb-sig-studio-cache').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './puntos.js',
-        'https://unpkg.com/leaflet/dist/leaflet.css',
-        'https://unpkg.com/leaflet/dist/leaflet.js'
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Archivos cacheados");
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
