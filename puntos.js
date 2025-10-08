3b1889f1359e2d1e915d49e914570b1fc3a7b9b3
Josue-210199 <jv2405471@gmail.com>
2025-10-07 23:12:54 -0500

    Create puntos.js

---
+++ puntos.js
@@ -0,0 +1,13 @@
+var puntosGeoJSON = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": { "name": "Punto 1" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-77.005, -12.032]
+      }
+    }
+  ]
+};
