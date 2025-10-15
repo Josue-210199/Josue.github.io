9730aced79cc66e584c2164781311b4778470cb8
Josue-210199 <jv2405471@gmail.com>
2025-10-15 17:25:01 -0500

    Update puntos.js

--- puntos.js
+++ puntos.js
@@ -3,11 +3,18 @@ var puntosGeoJSON = {
   "features": [
     {
       "type": "Feature",
-      "properties": { "name": "Punto 1" },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [-77.005, -12.032]
-      }
+      "properties": { "name": "Comisaría de Caja de Agua" },
+      "geometry": { "type": "Point", "coordinates": [-76.9825, -12.0103] }
+    },
+    {
+      "type": "Feature",
+      "properties": { "name": "Centro de Salud Canto Grande" },
+      "geometry": { "type": "Point", "coordinates": [-76.9750, -12.0251] }
+    },
+    {
+      "type": "Feature",
+      "properties": { "name": "Colegio San José Obrero - Refugio temporal" },
+      "geometry": { "type": "Point", "coordinates": [-76.9702, -12.0173] }
     }
   ]
 };
