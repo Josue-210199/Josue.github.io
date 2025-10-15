76047967b6de36027283707d4b2ea73d9cdcbe6e
Josue-210199 <jv2405471@gmail.com>
2025-10-15 17:25:16 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,34 @@
-const CACHE_NAME = "jb-sig-studio-v1";
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./puntos.js",
-  "https://unpkg.com/leaflet/dist/leaflet.css",
-  "https://unpkg.com/leaflet/dist/leaflet.js"
-];
+const CACHE_NAME = "jb-sig-mapa-v1";
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log("Archivos cacheados");
-      return cache.addAll(urlsToCache);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./puntos.js",
+        "https://unpkg.com/leaflet/dist/leaflet.js",
+        "https://unpkg.com/leaflet/dist/leaflet.css"
+      ]);
     })
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    caches.match(event.request).then((response) => {
+      // Si el recurso está en caché, lo usa; si no, lo descarga y guarda
+      return (
+        response ||
+        fetch(event.request)
+          .then((fetchRes) => {
+            return caches.open(CACHE_NAME).then((cache) => {
+              cache.put(event.request, fetchRes.clone());
+              return fetchRes;
+            });
+          })
+          .catch(() => caches.match("./index.html"))
+      );
     })
   );
 });
